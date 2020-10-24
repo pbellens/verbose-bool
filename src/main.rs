ea9c262ca027e59ce1279358bb41da42f704d618
Pieter Bellens <pbellens@gmail.com>
2020-10-24 23:48:53 +0200

    Add not and or condition

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,9 @@ impl Condition {
         (*self.expr)(self.name.to_string()) 
     }
 
-    fn new(n: String, e: Box<dyn Fn() -> bool>) -> Condition {
+    fn new(n: &str, e: Box<dyn Fn() -> bool>) -> Condition {
         Condition {
-            name: n,
+            name: n.to_string(),
             expr: Box::new(move |n| {
                 match e()
                 {
@@ -39,7 +39,7 @@ impl Condition {
 
     fn and(self, other: Condition) -> Condition {
         Condition {
-            name: format!("{} and {}", self.name, other.name),
+            name: format!("({} and {})", self.name, other.name),
             expr: Box::new(move |n| {
                 if let Truth { value: false, trace: Some(mut t) } = self.eval() {
                     t.push(n);
@@ -61,17 +61,50 @@ impl Condition {
             })
         }
     }
+
+    fn or(self, other: Condition) -> Condition {
+        Condition {
+            name: format!("({} or {})", self.name, other.name),
+            expr: Box::new(move |n| {
+                if let Truth { value: false, trace: _ } = self.eval() {
+                    if let Truth { value: false, trace: Some(mut t) } = other.eval() {
+                        t.push(n);
+                        return Truth {
+                            value: false,
+                            trace: Some(t)
+                        }
+                    }
+                }
+                Truth::new(true)
+            })
+        }
+    }
+
+    fn not(c: Condition) -> Condition {
+        Condition {
+            name: format!("(not {})", c.name),
+            expr: Box::new(move |n| {
+                if let Truth { value: true, trace: _ } = c.eval() {
+                    return Truth {
+                        value: false,
+                        trace: Some(vec![n])
+                    }
+                }
+                Truth::new(true)
+            })
+        }
+    }
 }
 
 fn main() {
-    let t1 = Condition::new("A".to_string(), Box::new(|| true));
-    let t2 = Condition::new("B".to_string(), Box::new(|| true));
-    let t3 = Condition::new("C".to_string(), Box::new(|| false));
-    let t4 = Condition::new("D".to_string(), Box::new(|| true));
+    let t1 = Condition::new("A", Box::new(|| true));
+    let t2 = Condition::new("B", Box::new(|| false));
+    let t3 = Condition::new("C", Box::new(|| true));
+    let t4 = Condition::new("D", Box::new(|| false));
 
-    //let c1 = t3.and(t4);
-    //let c2 = t1.and(t2);
-    let c = t1.and(t2).and(t3).and(t4);
+    let c1 = t3.or(t4);
+    let c2 = Condition::not(t1);
+    let c = c1.and(c2);
 
     let value = c.eval();
     println!("evaluated \"{}\" to {:?}", c.name, value.value);
